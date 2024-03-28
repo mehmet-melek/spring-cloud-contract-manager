44beb844619fe01e22886bf543ea04c9f6e7979a
mehmet-melek <mehmetmelekk@gmail.com>
2024-03-29 02:38:34 +0300

    db search, customer service and thread added

---
+++ src/main/java/com/melek/springcloudcontractmanager/order/OrderItem.java
@@ -0,0 +1,25 @@
+package com.melek.springcloudcontractmanager.order;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class OrderItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private Long orderId;
+    private Long productId;
+    private int quantity;
+    private double price;
+
+}
