aba06eabfa0998987a74eb6b3b50d005a176f6a5
mehmet-melek <mehmetmelekk@gmail.com>
2024-03-29 01:58:33 +0300

    order function and db operations added

--- src/main/java/com/melek/springcloudcontractmanager/SpringCloudContractManagerApplication.java
+++ src/main/java/com/melek/springcloudcontractmanager/SpringCloudContractManagerApplication.java
@@ -2,14 +2,35 @@ package com.melek.springcloudcontractmanager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
+
+import java.sql.*;
 
 @SpringBootApplication
 public class SpringCloudContractManagerApplication{
 
-    public static void main(String[] args) {
-        SpringApplication.run(SpringCloudContractManagerApplication.class, args);
+    public SpringCloudContractManagerApplication() throws SQLException {
+        Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/test", "root", "password");
+
+        PreparedStatement statement = connection.prepareStatement("SELECT * FROM users");
+
+        ResultSet resultSet = statement.executeQuery();
+
+        while (resultSet.next()) {
+            System.out.println(resultSet.getString("name"));
+        }
+
+        resultSet.close();
+        statement.close();
+        connection.close();
     }
 
+    public static void main(String[] args) {
+        try {
+            new SpringCloudContractManagerApplication();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        SpringApplication.run(SpringCloudContractManagerApplication.class, args);
+    }
 
 }
