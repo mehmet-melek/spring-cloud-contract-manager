9c5df5505e6a365ae76502699bc72c5682da4d5c
mehmet-melek <mehmetmelekk@gmail.com>
2024-03-29 00:04:00 +0300

    thread sample

--- src/main/java/com/melek/springcloudcontractmanager/SpringCloudContractManagerApplication.java
+++ src/main/java/com/melek/springcloudcontractmanager/SpringCloudContractManagerApplication.java
@@ -7,7 +7,33 @@ import org.springframework.context.annotation.ComponentScan;
 @SpringBootApplication
 public class SpringCloudContractManagerApplication {
 
+    private static int counter = 0;
+
+
     public static void main(String[] args) {
+        Thread thread1 = new Thread(() -> {
+            for (int i = 0; i < 1000; i++) {
+                counter++;
+            }
+        });
+
+        Thread thread2 = new Thread(() -> {
+            for (int i = 0; i < 1000; i++) {
+                counter++;
+            }
+        });
+
+        thread1.start();
+        thread2.start();
+
+        try {
+            thread1.join();
+            thread2.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("Counter: " + counter);
         SpringApplication.run(SpringCloudContractManagerApplication.class, args);
     }
 
